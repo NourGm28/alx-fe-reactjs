9c8f7f50d52300d1cd7f92513db98e337a67d1f0
NourGm28 <nournornor179@gmail.com>
2024-11-10 22:22:27 +0200

    updating react files

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{padding: '20px',color: 'darkblue', borderTop:'1px solid grey', borderBottom:'1px solid grey' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+
+export default MainContent;
