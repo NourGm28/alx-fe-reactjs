71e637a7dc86e9b8f549a40d00682d70b7c6fc1b
NourGm28 <nournornor179@gmail.com>
2024-11-25 00:28:47 +0200

    trying smth

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,13 @@ import create from 'zustand'
 
 const useRecipeStore = create(set => ({
   recipes: [],
+  searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  ["setSearchTerm","searchTerm"]
+  filteredRecipes: [],
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase()))
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
   updateRecipe: (upstate) => set(state => ({recipes:state.recipes.map((upstate) => 
@@ -12,6 +19,6 @@ deleteRecipe: (id) =>
   set((state) => ({
     recipes: state.recipes.filter((recipe) => recipe.id !== id),
   })),
+})),
 }));
-
 export { useRecipeStore };
