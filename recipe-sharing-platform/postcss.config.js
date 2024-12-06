3a731a8f14116939d879a74fe6111021f50d6611
NourGm28 <nournornor179@gmail.com>
2024-12-06 23:24:45 +0200

    creating files pages

--- recipe-sharing-platform/postcss.config.js
+++ recipe-sharing-platform/postcss.config.js
@@ -1,10 +1,6 @@
-module.exports = () => {
-  return {
-    plugins: [
-      require('autoprefixer')(),
-      require("postcss-combine-media-query"),
-      require("postcss-combine-duplicated-selectors"),
-      require("postcss-prettify"),
-    ],
-  };
-};
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
