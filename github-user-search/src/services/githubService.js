cb2c29b8aaa15aa815d2deca6622f881dab5e917
NourGm28 <nournornor179@gmail.com>
2024-12-15 23:12:44 +0200

    fixing bugs

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const fetchUserData = async ({ username, location, repos }) => {
+const fetchUserData = async ({ username, location,repos, minRepos }) => {
     try {
         let query = '';
         if (username) query += `${username}`;
         if (location) query += `+location:${location}`;
-        if (repos) query += `+repos:>${repos}`;
-        const res = await axios.get('https://api.github.com/users/${query}');
+        if (repos) minRepos += `+repos:>${repos}`;
+        const res = await axios.get('https://api.github.com/search/users?q');
         return res.data.items;
     } catch (error) {
         throw new Error(error,'User not found');
