ba2c118cb369bb63ebec52d501237a0e6341608f
NourGm28 <nournornor179@gmail.com>
2024-12-15 16:40:17 +0200

    building files and api's

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const onChange = (e) => {
+    console.log(e.target.value)
+  }
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(e.target.value)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>GitHub Users</h1>
+    <form onChange={onChange}>
+      <input type='search' placeholder='Search a User' />
+      <br />
+      <button style={{margin:'20px'}} onSubmit={onSubmit} type='submit'>Search</button>
+    </form>
     </>
   )
 }
