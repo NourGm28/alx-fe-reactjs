c1ed032e5dea39490266ce077b7aa5479c706c53
NourGm28 <nournornor179@gmail.com>
2025-01-18 01:51:29 +0200

    implementing files

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -11,12 +12,13 @@ const App = () => {
         <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> |{" "}
         <Link to="/blog/1">Blog Post 1</Link>
       </nav>
-
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile/*" element={<Profile />} />
         <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
+      </BrowserRouter>
     </div>
   );
 };
